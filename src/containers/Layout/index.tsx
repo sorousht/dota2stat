f2805ec71b9d1edcd48ad19946b13b9389b80a8f
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2019-01-02 18:08:21 +0330

    Improve layout and write a line in footer section

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,3 +1,4 @@
+import { Text } from "@blueprintjs/core";
 import classnames from "classnames";
 import * as React from "react";
 import "./bootstrap.css";
@@ -7,17 +8,25 @@ import styles from "./style.module.scss";
 export const Layout: React.SFC<{}> = (props) => {
   return (
     <div className={classnames(styles.layout, "bp3-dark")}>
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
           <div className="col-sm-12">
             <nav className="row">
               <Nav />
             </nav>
-            <section className="row">
-              {props.children}
-            </section>
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-12">
+                  {props.children}
+                </div>
+              </div>
+            </div>
             <footer className="row">
-              2018
+              <div className="col-sm-12 ">
+                <Text className="bp3-text-muted">
+                  Made with ❤️ by <a href="http://sorousht.com/" target="_blank">sorousht</a>
+                </Text>
+              </div>
             </footer>
           </div>
         </div>
