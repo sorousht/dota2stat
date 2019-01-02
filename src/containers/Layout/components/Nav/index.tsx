36cd37c5ce994dbdf700a5a0984b11439c25fe4b
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2019-01-02 13:56:53 +0330

    Add Nav component

---
+++ src/containers/Layout/components/Nav/index.tsx
@@ -0,0 +1,29 @@
+import { Alignment, Button, Navbar } from "@blueprintjs/core";
+import "flexboxgrid";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../../assets/images/dota-2_64.png";
+
+export const Nav: React.SFC<{}> = (props) => {
+  return (
+    <Navbar>
+      <Navbar.Group align={Alignment.LEFT}>
+        <Navbar.Heading>
+          <Link to="/">
+            <picture>
+              <img
+                alt="Dota 2 Statistics"
+                src={logo}
+                width="30"
+                height="30"
+              />
+            </picture>
+          </Link>
+        </Navbar.Heading>
+      </Navbar.Group>
+      <Navbar.Group align={Alignment.RIGHT}>
+        <Button className="bp3-minimal" icon="user" text="Profile" />
+      </Navbar.Group>
+    </Navbar>
+  );
+};
